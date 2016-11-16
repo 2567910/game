334de88b82132294db8ab2295506b6c87ab8013c
2567910 <luki.seyfarth@web.de>
2016-11-16 20:09:25 +0100

    Add files via upload

--- js/resources.js
+++ js/resources.js
@@ -60,7 +60,7 @@ $(document).ready(function() {
 
     setTimeout(function(){
         $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
+        $('h2').css('color','#fff');
+    }, 10000);
 
 });
